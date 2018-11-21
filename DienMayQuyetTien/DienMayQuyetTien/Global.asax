cbb3f4385402920047df901dad371322d8f15648
US <thjngking846@gmail.com>
2018-11-21 08:11:25 +0700

    add Database

---
+++ DienMayQuyetTien/DienMayQuyetTien/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DienMayQuyetTien.MvcApplication" Language="C#" %>
---
+++ DienMayQuyetTien/DienMayQuyetTien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DienMayQuyetTien.Startup))]
+namespace DienMayQuyetTien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
